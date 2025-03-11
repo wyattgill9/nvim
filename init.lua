072bd33bc5900a78d00b98ffc32668c72a3e475a
wyattgill9 <wyattgill2009@gmail.com>
2025-03-10 17:48:14 -0700

    e

--- init.lua
+++ init.lua
@@ -53,7 +53,9 @@ vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true })
 
+-- NO MORE MOUSEY
 
+vim.opt.mouse = ""
 
 -- Lazy
 
