7945bc1428aa468f7090416b209ee5d0789c8677
wyattgill9 <wyattgill2009@gmail.com>
2025-03-24 11:14:34 -0700

    e

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,7 +38,9 @@ return {
       lspconfig.gleam.setup({
 		capabilities = capabilities
       })
-
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
