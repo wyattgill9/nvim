eb5f64730761a48962d39b18cb2c459a4e231ae9
wyattgill9 <wyattgill2009@gmail.com>
2025-03-24 21:45:47 -0700

    rmeoved extra files & haskell language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -41,6 +41,10 @@ return {
       lspconfig.clangd.setup({
         capabilities = capabilities
       })
+      lspconfig.hls.setup({
+        capabilities = capabilities
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
