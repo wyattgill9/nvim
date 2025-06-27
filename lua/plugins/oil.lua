916530fc923a07bf00946fce7e11ff8266c2685a
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-06-26 21:34:29 -0700

    oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,8 +4,8 @@ return {
   ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.Add commentMore actions
   lazy = false,
 }
